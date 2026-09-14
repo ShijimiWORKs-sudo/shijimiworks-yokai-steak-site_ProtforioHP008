3573495d775a0974da78845ebbe8285676a52b2c
shijimiworks <shijimiworks.takaki@gmail.com>
2026-09-15 06:06:44 +0900

    Add GitHub Pages static export config and deploy workflow

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,12 @@
-/** @type {import('next').NextConfig} */
+﻿/** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  output: "export",
+  basePath: "/shijimiworks-yokai-steak-site_ProtforioHP008",
+  assetPrefix: "/shijimiworks-yokai-steak-site_ProtforioHP008/",
+  trailingSlash: true,
   images: {
+    unoptimized: true,
     remotePatterns: [
       {
         protocol: "https",
